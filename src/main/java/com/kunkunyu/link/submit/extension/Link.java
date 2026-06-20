3437369dddfff4041a5344e5ec88365d44482fa9
bbb-lsy07 <pitas_hadron0w@icloud.com>
2026-06-21 07:48:03 +0800

    适配链接管理2.2.x
    适配链接管理2.2.x

--- src/main/java/com/kunkunyu/link/submit/extension/Link.java
+++ src/main/java/com/kunkunyu/link/submit/extension/Link.java
@@ -1,20 +1,39 @@
 package com.kunkunyu.link.submit.extension;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import run.halo.app.extension.AbstractExtension;
 import run.halo.app.extension.GVK;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @GVK(group = "core.halo.run", version = "v1alpha1", kind = "Link", plural = "links", singular = "link")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Link extends AbstractExtension {
 
     private LinkSpec spec;
 
+    private Map<String, Object> additionalProperties = new LinkedHashMap<>();
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String key, Object value) {
+        this.additionalProperties.put(key, value);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return additionalProperties;
+    }
+
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class LinkSpec {
         @Schema(required = true)
         private String url;
@@ -29,5 +48,17 @@ public class Link extends AbstractExtension {
         private Integer priority;
 
         private String groupName;
+
+        private Map<String, Object> additionalProperties = new LinkedHashMap<>();
+
+        @JsonAnySetter
+        public void setAdditionalProperty(String key, Object value) {
+            this.additionalProperties.put(key, value);
+        }
+
+        @JsonAnyGetter
+        public Map<String, Object> getAdditionalProperties() {
+            return additionalProperties;
+        }
     }
 }
